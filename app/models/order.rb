7b0f42b09086b7dbbac810561cfec0d872c09e42
Gadjiev Akhmed <a.gadjiev1@mail.ru>
2025-01-24 21:13:36 +0300

    Remove status field from orders on html pages (in db its still exist but can be empty)

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,8 @@ class Order < ApplicationRecord
   validates :order_date, presence: true,  comparison: {greater_than_or_equal_to: 1900, less_than_or_equal_to: Date.today + 1, message: "cannot be in the future" }
   validates :delivery_address, presence: true, length: { maximum: 255 }
   validates :customer_name, presence: true, length: { maximum: 100 }
-  validates :status, presence: false, inclusion: { in: %w[pending shipped delivered canceled], message: "must be 'pending', 'shipped', 'delivered', or 'canceled'" }
+  validates :status, presence: false
+  # , inclusion: { in: %w[pending shipped delivered canceled], message: "must be 'pending', 'shipped', 'delivered', or 'canceled'" }
 
   def to_s_for_collapse_choice
     "#{order_date} #{delivery_address} #{customer_name}"
